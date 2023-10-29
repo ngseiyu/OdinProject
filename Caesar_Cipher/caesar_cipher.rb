07c8266932c1ae27c2f40638a2807d41130f8aae
Ng Sei Yu <44496936+ngseiyu@users.noreply.github.com>
2023-10-29 14:59:35 +0800

    Modified indentation of code

--- Caesar_Cipher/caesar_cipher.rb
+++ Caesar_Cipher/caesar_cipher.rb
@@ -1,15 +1,14 @@
 def caesar_cipher(str, factor)
-    small_letters = ('a'..'z').to_a
-    capital_letters = ('A'..'Z').to_a
+  small_letters = ('a'..'z').to_a
+  capital_letters = ('A'..'Z').to_a
 
-    str.chars.map do |c|
-        if small_letters.include?(c)
-            c = small_letters[(small_letters.find_index(c) + factor) % 26 ]
-        elsif capital_letters.include?(c)
-            # c = capital_letters[capital_letters.find_index(c) + factor - capital_letters.length]
-            c = capital_letters[(capital_letters.find_index(c) + factor) % 26]
-        else
-            c = c
-        end
-    end.join()
+  str.chars.map do |c|
+    if small_letters.include?(c)
+      c = small_letters[(small_letters.find_index(c) + factor) % 26 ]
+    elsif capital_letters.include?(c)
+      c = capital_letters[(capital_letters.find_index(c) + factor) % 26]
+    else
+      c = c
+    end
+  end.join()
 end
